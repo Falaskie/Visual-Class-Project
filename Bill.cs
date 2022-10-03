8765c7c32ca3edc0cdcbf012f58d9ff8de80a359
Falaskie <75765282+Falaskie@users.noreply.github.com>
2022-10-02 21:33:15 -0500

    variables

--- Bill.cs
+++ Bill.cs
@@ -1,15 +1,30 @@
 using System;
+using System.Ling;
+using System.Collections.Generic;
 
-class Bill
+namespace Bill
+
+
+Public class Bill
 {
-    static void Main(string[] args)
+   public static void Main(string[] args)
     {
 
         string HolderName = " ", HolderAddress = " ";
         int PremNum = 0, AccNum = 0, CurrRead = 0, PrevRead = 0,;
         double PrevBal = 0.0, Payments = 0.0, BalBroFor = 0.0, CurrChar = 0.0, TotAmDue = 0.0, Taxes = 0.0, Rate = 0.0, EarlyDis = 0.0, CurrUsage = 0.0,CurrPeriChar 0.0;
 
-   
+        Console.Writeline("Please enter customer's current meter reqding");
+
+
+
+    Console.WriteLine("Please Enter the pervious meter reading.");
+
+
+    Console,WriteLine("Please enter current usage rate");
+
+
+        Console.WriteLine("Please enter the Fuel Adjustment Rate");
     
     }
 }
