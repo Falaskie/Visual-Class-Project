c04c19b7e86693ca4c8ddcf4c19d0674c94ea293
Falaskie <75765282+Falaskie@users.noreply.github.com>
2022-10-02 22:32:36 -0500

    new file

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string HolderName = " ", HolderAddress = " ";
+int PremNum = 0, AccNum = 0, CurrRead = 0, PrevRead = 0;
+double PrevBal = 0.0, Payments = 0.0, BalBroFor = 0.0, CurrChar = 0.0, TotAmDue = 0.0, Taxes = 0.0, Rate = 0.0, EarlyDis = 0.0, CurrUsage = 0.0, CurrPeriChar = 0.0;
+
+Console.WriteLine("Please enter customer's current meter reading");
+
+
+
+Console.WriteLine("Please Enter the pervious meter reading.");
+
+
+Console.WriteLine("Please enter current usage rate");
+
+
+Console.WriteLine("Please enter the Fuel Adjustment Rate");
