3ec4acef4bdb87c47b8d6f355049da3567c0f1a8
Falaskie <75765282+Falaskie@users.noreply.github.com>
2022-10-02 21:11:27 -0500

    variables

--- Bill.cs
+++ Bill.cs
@@ -5,7 +5,9 @@ class Bill
     static void Main(string[] args)
     {
 
-
+        string HolderName = " ", HolderAddress = " ";
+        int PremNum = 0, AccNum = 0, CurrRead = 0, PrevRead = 0,;
+        double PrevBal = 0.0, Payments = 0.0, BalBroFor = 0.0, CurrChar = 0.0, TotDue = 0.0, Taxes = 0.0, Rate = 0.0;
 
     }
 }
