6d0aac35738985ca001c163b2b30633f7aab55ca
Falaskie <75765282+Falaskie@users.noreply.github.com>
2022-10-08 17:17:21 -0500

    classes

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,10 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Welcome to the Bill Payment System");
 
-string HolderName = " ", HolderAddress = " ";
-int PremNum = 0, AccNum = 0, CurrRead = 0, PrevRead = 0;
+string HolderName = " ", HolderAddress = " ", PremNum = " ", AccNum = " ";
+int  CurrRead = 0, PrevRead = 0, CustCharge = 455;
 double PrevBal = 0.0, Payments = 0.0, BalBroFor = 0.0, CurrChar = 0.0, TotAmDue = 0.0,
-    Taxes = 0.0, Rate = 0.0, EarlyDis = 0.0, CurrUsage = 0.0, CurrPeriChar = 0.0, FuelRate = 0.0, GCTcharge = 0.0;
+    Taxes = 0.0, Rate = 9.66, EarlyDis = 250.00, CurrUsage = 0.0, CurrPeriChar = 0.0, FuelRate = 0.0, GCTcharge = 0.0;
 
 Console.WriteLine("Please enter customer's current meter reading");
 CurrRead = Convert.ToInt32(Console.ReadLine());
