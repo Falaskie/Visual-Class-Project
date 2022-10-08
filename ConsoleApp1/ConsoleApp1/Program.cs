f9fadf043a720b215b06817f8392ccdb640cc7ce
Falaskie <75765282+Falaskie@users.noreply.github.com>
2022-10-08 16:32:41 -0500

    readlines

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,18 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Welcome to the Bill Payment System");
 
 string HolderName = " ", HolderAddress = " ";
 int PremNum = 0, AccNum = 0, CurrRead = 0, PrevRead = 0;
-double PrevBal = 0.0, Payments = 0.0, BalBroFor = 0.0, CurrChar = 0.0, TotAmDue = 0.0, Taxes = 0.0, Rate = 0.0, EarlyDis = 0.0, CurrUsage = 0.0, CurrPeriChar = 0.0;
+double PrevBal = 0.0, Payments = 0.0, BalBroFor = 0.0, CurrChar = 0.0, TotAmDue = 0.0,
+    Taxes = 0.0, Rate = 0.0, EarlyDis = 0.0, CurrUsage = 0.0, CurrPeriChar = 0.0, FuelRate = 0.0;
 
 Console.WriteLine("Please enter customer's current meter reading");
-
-
+CurrRead = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Please Enter the pervious meter reading.");
-
+PrevRead = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Please enter current usage rate");
-
+CurrUsage = Convert.ToDouble(Console.ReadLine());
 
 Console.WriteLine("Please enter the Fuel Adjustment Rate");
+FuelRate = Convert.ToDouble(Console.ReadLine());
+
