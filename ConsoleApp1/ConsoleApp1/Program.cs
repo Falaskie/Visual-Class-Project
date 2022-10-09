5f3a6104ffd3ab6b1bf90c3627d66ad2ca9eddd3
Falaskie <75765282+Falaskie@users.noreply.github.com>
2022-10-09 01:47:28 -0500

    class

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -8,8 +8,9 @@ int  CurrRead = 0, PrevRead = 0, CustCharge = 455;
 double PrevBal = 0.0, Payments = 0.0, BalBroFor = 0.0, CurrChar = 0.0, TotAmDue = 0.0,
     Taxes = 0.0, Rate = 9.66, EarlyDis = 250.00, CurrUsage = 0.0, CurrPeriChar = 0.0, FuelRate = 0.0, GCTcharge = 0.0,
     Subtotal = 0.0, FuelIPP = 0.0, FEAdjust = 0.0;
+Console.SetWindowSize(100, 25);
 
-
+/*
 //Entering of informations
 Console.WriteLine("Please enter customer's current meter reading");
 CurrRead = Convert.ToInt32(Console.ReadLine());
@@ -22,6 +23,7 @@ CurrUsage = Convert.ToDouble(Console.ReadLine());
 
 Console.WriteLine("Please enter the Fuel Adjustment Rate");
 FuelRate = Convert.ToDouble(Console.ReadLine());
+*/
 
 //Customer Information
 CustomerInfo customer = new CustomerInfo();
@@ -37,4 +39,7 @@ FEAdjust = Subtotal * FuelRate;
 
 Console.Clear();
 
-Console.WriteLine("Account # ", customer.AccNum);
+Console.WriteLine("Account # " +  customer.AccNum);
+Console.WriteLine("Name       " + customer.FirstName + customer.LastName);
+Console.WriteLine("Premises # " + customer.PremNum);
+Console.WriteLine("*************************************************************************************************** /n");
