8d8bd4489a30644bbab102eca83fcefdad8baf11
Falaskie <75765282+Falaskie@users.noreply.github.com>
2022-10-08 20:34:35 -0500

    before tax class

---
+++ ConsoleApp1/ConsoleApp1/BeforeTax.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BillPay
+{
+    public class BeforeTax
+    {
+        double Subtotal = 0.0;
+        double FEAdjust = 0.0;
+        double FuelIPP = 0.0;
+        double result = 0.0;
+        public BeforeTax(double subtotal, double fEAdjust, double fuelIPP)
+        {
+            Subtotal = subtotal;
+            FEAdjust = fEAdjust;
+            FuelIPP = fuelIPP;
+
+            result = Subtotal + FEAdjust + FuelIPP;
+            
+            return result; 
+        }   
+    }
+}
